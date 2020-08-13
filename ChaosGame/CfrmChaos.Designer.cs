4bd79025db025aec9185ffbb838e4be293d4ec7c
RapelangLepeli <50484534+RapelangLepeli@users.noreply.github.com>
2020-08-13 22:39:42 +0200

    Add files via upload

---
+++ ChaosGame/CfrmChaos.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ChaosGame
+{
+    partial class CfrmChaos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlDraw = new System.Windows.Forms.Panel();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pnlDraw
+            // 
+            this.pnlDraw.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pnlDraw.Location = new System.Drawing.Point(1, 1);
+            this.pnlDraw.Name = "pnlDraw";
+            this.pnlDraw.Size = new System.Drawing.Size(609, 630);
+            this.pnlDraw.TabIndex = 0;
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(250, 637);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(75, 23);
+            this.btnDraw.TabIndex = 1;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // CfrmChaos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 661);
+            this.Controls.Add(this.btnDraw);
+            this.Controls.Add(this.pnlDraw);
+            this.Name = "CfrmChaos";
+            this.Text = "Chaos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlDraw;
+        private System.Windows.Forms.Button btnDraw;
+    }
+}
+
---
+++ ChaosGame/CfrmChaos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChaosGame
+{
+    public partial class CfrmChaos : Form
+    {
+        static Point PointA;
+        static Point PointB;
+        static Point PointC;
+        static Point StartPoint;
+
+        static Graphics draw;
+
+        SolidBrush BlackSolidBrush;
+        SolidBrush RedSolidBrush;
+
+        static Random Die;
+
+        public CfrmChaos()
+        {
+            InitializeComponent();
+            draw = pnlDraw.CreateGraphics();
+
+            PointA.X = pnlDraw.Width / 2;
+            PointA.Y = pnlDraw.Top + 3;
+
+            PointB.X = pnlDraw.Width * 10 / 100;
+            PointB.Y = pnlDraw.Height * 90 / 100;
+
+            PointC.X = pnlDraw.Width * 90 / 100;
+            PointC.Y = pnlDraw.Height * 90 / 100;
+
+            StartPoint.X = PointA.X;
+            StartPoint.Y = PointA.Y + 60;
+
+            BlackSolidBrush = new SolidBrush(Color.Black);
+            RedSolidBrush = new SolidBrush(Color.Red);
+
+            Die = new Random((int)DateTime.Now.Ticks);
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            DrawPoints();
+            for (int i = 0; i < 100; i++)
+            {
+                int DieNumber = Die.Next(1,7);
+                draw.FillEllipse(RedSolidBrush,StartPoint.X,StartPoint.Y,4,4);
+                if (DieNumber == 1 || DieNumber == 2)
+                {
+                    StartPoint.X = (StartPoint.X + PointA.X)/2;
+                    StartPoint.Y = (StartPoint.Y + PointA.Y) / 2;
+                }
+                else if (DieNumber == 3 || DieNumber == 4)
+                {
+                    StartPoint.X = (StartPoint.X + PointB.X) / 2;
+                    StartPoint.Y = (StartPoint.Y + PointB.Y) / 2;
+
+                }
+                else
+                {
+                    StartPoint.X = (StartPoint.X + PointC.X) / 2;
+                    StartPoint.Y = (StartPoint.Y + PointC.Y) / 2;
+
+                }
+            }
+        }
+        private void DrawPoints()
+        {
+            draw.FillEllipse(BlackSolidBrush,PointA.X,PointA.Y,6,6);
+            draw.FillEllipse(BlackSolidBrush, PointB.X, PointB.Y, 6, 6);
+            draw.FillEllipse(BlackSolidBrush, PointC.X, PointC.Y, 6, 6);
+            draw.FillEllipse(RedSolidBrush,StartPoint.X,StartPoint.Y,4,4);
+        }
+    }
+}
